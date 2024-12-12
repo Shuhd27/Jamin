06074265ad96f512a17833877c389b3b08705561
Shuhd27 <shuhdalazzani27@gmail.com>
2024-12-12 10:37:25 +0100

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * De database verbindingsgegevens
+ */
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'jamin_b');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+
+/**
+ * De naam van de virtualhost
+ */
+define('URLROOT', 'http://jamin.nl');
+
+
+/**
+ * Het pad naar de folder app
+ */
+define('APPROOT', dirname(dirname(__FILE__)));
+
+/**
+ * Geef aan hoeveel items er per pagina getoond moeten worden
+ */
+define('LIMIT', 3);
